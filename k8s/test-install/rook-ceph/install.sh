17c09b34ce8338e8352acff879da3ad85822792f
smalle <oldinaction@qq.com>
2019-10-17 13:58:34 +0800

    Update install.sh

--- k8s/test-install/rook-ceph/install.sh
+++ k8s/test-install/rook-ceph/install.sh
@@ -1,5 +1,6 @@
 cd /root/k8s/rook-1.1.2/cluster/examples/kubernetes/ceph
 kubectl apply -f common.yaml
+kubectl apply -f https://raw.githubusercontent.com/oldinaction/scripts/master/k8s/prod/allow-tz-env.yaml -n rook-ceph
 kubectl apply -f operator.yaml
 kubectl apply -f cluster.yaml
 kubectl apply -f dashboard-external-https.yaml
