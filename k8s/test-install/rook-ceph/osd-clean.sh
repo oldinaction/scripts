c81544b2d9034426e35e8615c7e8fa08f26ecd8b
smalle <oldinaction@qq.com>
2019-10-17 12:19:26 +0800

    Update osd-clean.sh

--- k8s/test-install/rook-ceph/osd-clean.sh
+++ k8s/test-install/rook-ceph/osd-clean.sh
@@ -1,7 +1,12 @@
-rm -rf /var/lib/rook
-sgdisk --zap-all /dev/sdb
-/usr/sbin/wipefs --all /dev/sdb
+#!/bin/bash
+
+sudo rm -rf /var/lib/rook
+# 卸载rook数据目录
+sudo umount /data/rook
+sudo sgdisk --zap-all /dev/vdb
+sudo /usr/sbin/wipefs --all /dev/vdb
 # 在每个节点上删除映射
-ls /dev/mapper/ceph-* | xargs -I% -- dmsetup remove %
-rm -rf /dev/mapper/ceph-*
-rm -rf /dev/ceph-*
+sudo ls /dev/mapper/ceph-* | xargs -I% -- dmsetup remove %
+sudo rm -rf /dev/mapper/ceph-*
+sudo rm -rf /dev/ceph-*
+sudo rm -rf /data/rook
